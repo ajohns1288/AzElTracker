3b80300aa07f3ea82d7b90d164b2c62af0d7455c
ajohns1288 <95730335+ajohns1288@users.noreply.github.com>
2022-03-11 19:17:29 -0500

    Documentation update

--- rotorParams.h
+++ rotorParams.h
@@ -17,14 +17,16 @@
 #define EL_STEP_PIN 3
 #define EL_DIR_PIN 2
 
+//These are settings for the home sensors. You may need to change these to get homing working correctly based on the specific sensor used.
+//NOTE: Hall effect sensors are sensitive to which magnetic pole is present.
 #define EL_HOME_PIN_DIG 7 //Digital home pin
 #define EL_HOME_PIN_ANL A0 //Analog home pin
-#define INVERT_DIG_HOME 0 //0: Home switch high when at home, 1: Home switch low when at home
-#define ANALOG_HOME_VAL 700
+#define INVERT_DIG_HOME 0 //Used to account for different sensor styles/pullups. 0: Home switch high when at home, 1: Home switch low when at home
+#define ANALOG_HOME_VAL 700 //A2D value above which the rotor is considered "homed"
 
 
-#define POS_HYST 80 //Hysteresis (deg*100)
-#define HOME_MAX_MOVEMENT 7000 //Maximum movement during homing (deg*100)
+#define POS_HYST 80 //Hysteresis (deg*100), default is 0.8 deg. If the commanded position is within this amount of the actual, no movement is made.
+#define HOME_MAX_MOVEMENT 7000 //Maximum movement during homing (deg*100). Default is 70 degrees. This is used to prevent the rotor from over extending if home switch is missing.
 
 
 //home
